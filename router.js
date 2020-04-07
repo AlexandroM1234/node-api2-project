da0fc03c60e57f46c69d7d19483e3b79fbc570d8
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-07 15:58:13 -0400

    comments get works

--- router.js
+++ router.js
@@ -39,4 +39,26 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// router gets id for comments
+
+router.get("/:id/comments", (req, res) => {
+  Posts.findPostComments(req.params.id)
+    .then((comments) => {
+      if (comments) {
+        res.status(200).json(comments);
+      } else if (!comments) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res
+          .status(500)
+          .json({ error: "The comments information could not be retrieved." });
+      }
+    })
+    .catch((err) => {
+      console.log("messed up comments get", err);
+    });
+});
+
 module.exports = router;
